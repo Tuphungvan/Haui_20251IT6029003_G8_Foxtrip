ec865dd85290fab3ebc9d10e805702d499a94af6
nguyenVu204 <nqv0126530@gmail.com>
2025-11-17 22:14:35 +0700

    search Tour

--- App/app/src/main/java/vn/androidhaui/travelapp/models/Wallet.java
+++
@@ -1,9 +0,0 @@
-package vn.androidhaui.travelapp.models;
-
-public class Wallet {
-    private String userId;
-    private Double balance;
-
-    public String getUserId() { return userId; }
-    public Double getBalance() { return balance != null ? balance : 0.0; }
-}
